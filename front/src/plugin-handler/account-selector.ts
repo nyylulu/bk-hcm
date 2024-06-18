acc635ab66061397807e92ba9a7d7d9d151b04f2
royalpioneer <royalpioneer@foxmail.com>
2024-06-18 18:16:08 +0800

    feat: 修复编译失败问题

--- front/src/plugin-handler/account-selector.ts
+++ front/src/plugin-handler/account-selector.ts
@@ -3,3 +3,5 @@ import { VendorEnum } from '@/common/constant';
 export const filterAccountList = (accountList: any[]) => {
   return accountList.filter((item) => item.vendor === VendorEnum.TCLOUD);
 };
+
+export type FilterAccountListHandler = typeof filterAccountList;
