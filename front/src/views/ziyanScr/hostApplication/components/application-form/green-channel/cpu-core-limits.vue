f10646d36eb92b5bb5a408097cb413922b2e9daf
v_cwcochen <v_cwcochen@tencent.com>
2024-11-20 03:32:46 +0000

    feat: 小额绿通-主机申请联调 (merge request !1753)
    --story=120677497

---
+++ front/src/views/ziyanScr/hostApplication/components/application-form/green-channel/cpu-core-limits.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { computed, onBeforeMount } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useI18n } from 'vue-i18n';
+import { useGreenChannelQuotaStore } from '@/store/green-channel/quota';
+
+defineOptions({ name: 'green-channel-cpu-core-limits' });
+defineProps<{
+  replicasCpuCores: number;
+}>();
+
+const { t } = useI18n();
+const greenChannelQuotaStore = useGreenChannelQuotaStore();
+const { globalQuotaConfig } = storeToRefs(greenChannelQuotaStore);
+
+const availableCpuCoreQuota = computed(() => {
+  const { biz_quota = 0, sum_delivered_core = 0 } = globalQuotaConfig.value ?? {};
+  return biz_quota - sum_delivered_core;
+});
+const cpuCoreQuota = computed(() => globalQuotaConfig.value?.biz_quota);
+
+onBeforeMount(() => {
+  greenChannelQuotaStore.getGlobalQuota();
+});
+
+defineExpose({ availableCpuCoreQuota });
+</script>
+
+<template>
+  <ul class="wrapper">
+    <li>
+      <span>{{ t('本周剩余核数：') }}</span>
+      <span class="number">{{ availableCpuCoreQuota }} {{ t('核') }}</span>
+    </li>
+    <li>
+      <span>{{ t('本周限额核数：') }}</span>
+      <span class="number">{{ cpuCoreQuota }} {{ t('核') }}</span>
+    </li>
+    <li>
+      <span>{{ t('需求核数：') }}</span>
+      <span class="number">{{ replicasCpuCores }} {{ t('核') }}</span>
+    </li>
+  </ul>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+  margin-left: 24px;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+
+  .number {
+    color: $warning-color;
+  }
+}
+</style>
---
+++ front/src/views/ziyanScr/hostApplication/components/application-form/green-channel/tips-alert.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import wName from '@/components/w-name';
+
+defineOptions({ name: 'green-channel-tips-alert' });
+
+const { t } = useI18n();
+</script>
+
+<template>
+  <bk-alert>
+    <p>
+      {{
+        t(
+          '注意：当前选择的是小额绿通，用于突发少量资源申领、紧急申领的场景，无需提前报需求预测。每单申领 100 核心以下时不需要审批，大于 100 核心时需要业务 leader 及资源团队审批。',
+        )
+      }}
+    </p>
+    <p>
+      {{ t('如有疑问，请咨询') }}
+      <w-name name="ICR" :alias="t('ICR(IEG资源服务助手)')" />
+      {{ t('确认。') }}
+    </p>
+  </bk-alert>
+</template>
