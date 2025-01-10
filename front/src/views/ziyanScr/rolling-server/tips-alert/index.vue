081767f7104549e6353a3e56c3992e636c3f57df
yunyaoyang <yunyaoyang@tencent.com>
2025-01-10 02:58:08 +0000

    chore: 主机申请滚服项目提示文案调整 (merge request !1987)
    Squash merge branch 'chore-hostapply-rollsvrtips' into 'bcc/v1.7.x'
    chore: 主机申请滚服项目提示文案调整
    
    --story=121609403
    
    
    TAPD: --story=121609403

--- front/src/views/ziyanScr/rolling-server/tips-alert/index.vue
+++ front/src/views/ziyanScr/rolling-server/tips-alert/index.vue
@@ -13,9 +13,9 @@ const { t } = useI18n();
   <Alert class="rolling-server-tips-alert" theme="warning">
     <p>{{ t('注意：当前选择的是滚服项目，每个业务每月只允许申请使用1万核心，额度有限，限量申请，先到先得。更多信息请参考文档') }} <bk-link theme="primary" href="https://iwiki.woa.com/p/4012608772" target="_blank">https://iwiki.woa.com/p/4012608772</bk-link></p>
     <ul>
-      <li><strong>{{ t('套餐计费：') }}</strong>{{ (t('继承原有机器的计费起止时间、计费模式')) }}</li>
+      <li><strong>{{ t('套餐计费：') }}</strong>{{ t('继承原有机器的计费起止时间、计费模式') }}</li>
       <li><strong>{{ t('机型限制：') }}</strong>{{ t('只支持通用机型（通用/专用机型，请参考公司定义说明') }} <bk-link theme="primary" href="https://doc.weixin.qq.com/sheet/e3_ACAAPwaDACcY182EYUTTh6Fw0xsO9?scode=AJEAIQdfAAooKA1w6rAJwAVAbdAFw&tab=BB08J2" target="_blank">https://doc.weixin.qq.com/sheet/e3_ACAAPwaDACcY182EYUTTh6Fw0xsO9?scode=AJEAIQdfAAooKA1w6rAJwAVAbdAFw&tab=BB08J2</bk-link>{{ t('）') }}</li>
-      <li><strong>{{ t('地域限制：') }}</strong>{{ t('继承原有机器的大区，如原机器在华东，则可以选择华东区的上海，南京等城市，其他地域华南区（深圳、广州、清远等），华北（天津），西南（重庆）规则相同。') }}</li>
+      <li><strong>{{ t('地域限制：') }}</strong>{{ t('支持同城内所有可用区继承，如继承机器是上海的机器，可提交所有上海可用区机器申领') }}</li>
       <li><strong>{{ t('退回要求：') }}</strong>{{ t('应在30天内退回等量的相应要求的机器，超期会产生罚金') }}</li>
       <li><strong>{{ t('超期罚金：') }}</strong>{{ t('在机器申请后第31-121天内未退还，按超期机器的50%成本核算加收罚金，最少收1天，最多收91天') }}</li>
     </ul>
