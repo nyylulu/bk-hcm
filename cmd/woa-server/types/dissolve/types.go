08cec0cf9da474541b84943200d311b43139180a
connorzheng <connorzheng@tencent.com>
2025-02-13 03:11:42 +0000

    feat: 调整裁撤主机最大导出数量 (merge request !2019)
    Squash merge branch 'v1.7.x-fix-dissolve-bug' into 'bcc/v1.7.x'
    feat: 调整裁撤主机最大导出数量
    
     
    
    
    
    TAPD: --story=121904865

--- cmd/woa-server/types/dissolve/types.go
+++ cmd/woa-server/types/dissolve/types.go
@@ -184,7 +184,7 @@ func (req *HostListReq) Validate() error {
 
 	pageOpt := &core.PageOption{
 		EnableUnlimitedLimit: false,
-		MaxLimit:             10000, // 由于前端需要导出数据，这里特殊调整限制值
+		MaxLimit:             50000, // 由于前端需要导出数据，这里特殊调整限制值
 		DisabledSort:         false,
 	}
 	if err := req.Page.Validate(pageOpt); err != nil {
