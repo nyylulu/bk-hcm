5c237e02e44fd57c891b3e139b73b09c1ec66df2
connorzheng <connorzheng@tencent.com>
2025-05-14 02:24:46 +0000

    fix: 修复cvm生产从cc查不到主机时，添加到本地的的主机没有固资号的问题 (merge request !2210)
    Squash merge branch 'v1.7.x-fix-add-device' into 'bcc/v1.7.x'
    fix: 修复cvm生产从cc查不到主机时，添加到本地的的主机没有固资号的问题
    
     
    
    
    
    TAPD: --story=123907280

--- cmd/woa-server/logics/task/scheduler/generator/generator.go
+++ cmd/woa-server/logics/task/scheduler/generator/generator.go
@@ -1156,6 +1156,7 @@ func (g *Generator) buildDevicesInfo(items []*types.DeviceInfo, order *types.App
 			GenerateId:   generateId,
 			BkBizId:      int(order.BkBizId),
 			User:         order.User,
+			AssetId:      item.AssetId,
 			Ip:           item.Ip,
 			RequireType:  order.RequireType,
 			ResourceType: order.ResourceType,
@@ -1181,9 +1182,9 @@ func (g *Generator) buildDevicesInfo(items []*types.DeviceInfo, order *types.App
 		}
 		// add device detail info from cc
 		if host, ok := mapAssetIDToHost[item.AssetId]; !ok {
-			logs.Warnf("failed to get %s detail info in cc", item.AssetId)
+			logs.Warnf("failed to get host detail info in cc, subOrderID: %s, assetID: %s", order.SubOrderId,
+				item.AssetId)
 		} else {
-			device.AssetId = host.BkAssetID
 			// update device type from cc
 			device.DeviceType = host.SvrDeviceClass
 			device.ZoneName = host.SubZone
