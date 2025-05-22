ed35fb3c0ca0188e216391f09a853cf9d637d204
v_cwcochen <v_cwcochen@tencent.com>
2025-05-22 03:14:07 +0000

    feat: CLB-账号下-同步按钮-云厂商增加自研云 --story=124051198 (merge request !2237)
    Squash merge branch 'bcc/feat-v1.8.x#124051198' into 'bcc/v1.8.x'
    
     
    
    
    
    TAPD: --story=124051198

--- front/src/components/account-selector/resource-cond-internal.plugin.ts
+++
@@ -1,4 +0,0 @@
-import { QueryRuleOPEnum, type RulesItem } from '@/typings';
-import { VendorEnum } from '@/common/constant';
-
-export const resourceCond: RulesItem[] = [{ op: QueryRuleOPEnum.NEQ, field: 'vendor', value: VendorEnum.ZIYAN }];
