811fe3a40b07e7216c7626ad6dfcaf7542652dc4
v_myygnie <2159685130@qq.com>
2025-10-16 11:05:21 +0800

    fix: 创建7层监听器-监听器不存在时也需要校验证书是否在业务下--story=127778144

--- cmd/cloud-server/logics/load-balancer/create_layer7_listener_preview_executor.go
+++ cmd/cloud-server/logics/load-balancer/create_layer7_listener_preview_executor.go
@@ -210,6 +210,13 @@ func (c *CreateLayer7ListenerPreviewExecutor) validateWithDB(kt *kit.Kit, cloudI
 
 func (c *CreateLayer7ListenerPreviewExecutor) validateListener(kt *kit.Kit, detail *CreateLayer7ListenerDetail) error {
 
+	//校验https协议是否有证书
+	if detail.Protocol == enumor.HttpsProtocol {
+		if err := c.validateCert(kt, detail); err != nil {
+			return err
+		}
+	}
+
 	switch c.vendor {
 	case enumor.TCloud:
 		listeners, err := c.getTCloudListenersByPort(kt, detail.CloudClbID, detail.ListenerPorts[0])
@@ -333,6 +340,7 @@ func (c *CreateLayer7ListenerPreviewExecutor) validateCert(kt *kit.Kit, curDetai
 			curDetail.Status.SetNotExecutable()
 			curDetail.ValidateResult = append(curDetail.ValidateResult, fmt.Sprintf("cert(%s) not found", cloudID))
 		}
+		return fmt.Errorf("certificate(%s) not found in business", cloudID)
 	}
 	return nil
 }
