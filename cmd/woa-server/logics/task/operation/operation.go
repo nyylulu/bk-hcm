f5b45af235cac2d3a2c52f90bf8de43cd3bb0baa
v_myygnie <2159685130@qq.com>
2025-11-28 16:42:06 +0800

    fix: 申请单据比例-首页-“结单率”接口 --story=128355490

--- cmd/woa-server/logics/task/operation/operation.go
+++ cmd/woa-server/logics/task/operation/operation.go
@@ -379,7 +379,11 @@ func buildCompletionRateStatisticsPipeline(filter map[string]interface{}) []map[
 					"date":   "$create_at"}},
 			"is_done": map[string]interface{}{
 				"$cond": []interface{}{
-					map[string]interface{}{"$eq": []interface{}{"$stage", "DONE"}},
+					map[string]interface{}{
+						condition.BKDBIN: []interface{}{
+							"$stage", []interface{}{types.TicketStageDone, types.TicketStageTerminate},
+						},
+					},
 					1,
 					0,
 				},
@@ -401,7 +405,7 @@ func buildCompletionRateStatisticsPipeline(filter map[string]interface{}) []map[
 									"$done_count",
 									map[string]interface{}{
 										"$cond": []interface{}{
-											map[string]interface{}{"$eq": []interface{}{"$total_count", 0}},
+											map[string]interface{}{condition.BKDBEQ: []interface{}{"$total_count", 0}},
 											1,
 											"$total_count",
 										},
@@ -515,8 +519,16 @@ func buildCompletionRateDetailPipeline(baseFilter map[string]interface{}) []map[
 					"$cond": []interface{}{
 						map[string]interface{}{
 							"$and": []interface{}{
-								map[string]interface{}{"$eq": []interface{}{"$stage", types.TicketStageDone}},
-								map[string]interface{}{"$eq": []interface{}{"$status", types.ApplyStatusDone}},
+								map[string]interface{}{
+									condition.BKDBIN: []interface{}{
+										"$stage", []interface{}{types.TicketStageDone, types.TicketStageTerminate},
+									},
+								},
+								map[string]interface{}{
+									condition.BKDBIN: []interface{}{
+										"$status", []interface{}{types.ApplyStatusDone, types.ApplyStatusTerminate},
+									},
+								},
 							},
 						},
 						1,
@@ -539,7 +551,7 @@ func buildCompletionRateDetailPipeline(baseFilter map[string]interface{}) []map[
 			"$addFields": map[string]interface{}{
 				"completion_rate": map[string]interface{}{
 					"$cond": []interface{}{
-						map[string]interface{}{"$eq": []interface{}{"$total_orders", 0}},
+						map[string]interface{}{condition.BKDBEQ: []interface{}{"$total_orders", 0}},
 						0.0,
 						map[string]interface{}{
 							"$multiply": []interface{}{
