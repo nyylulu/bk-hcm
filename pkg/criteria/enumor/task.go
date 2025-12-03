a99be425baf25c47c18acd290be9b5b6157bccf7
v_myygnie <2159685130@qq.com>
2025-12-03 17:12:10 +0800

    fix: 交付耗时统计-结单率及时区问题确认 --story=128997378

--- pkg/criteria/enumor/task.go
+++ pkg/criteria/enumor/task.go
@@ -174,11 +174,3 @@ const (
 	// TaskListenerAddTarget is a task indicating that add target to listener rule.
 	TaskListenerAddTarget TaskOperation = "listener_add_target"
 )
-
-// ApplyTicketSource is apply ticket source
-type ApplyTicketSource string
-
-const (
-	// ApplyTicketSrcPurchaseToResPool 采购到资源池的订单
-	ApplyTicketSrcPurchaseToResPool ApplyTicketSource = "purchase_to_resource_pool"
-)
